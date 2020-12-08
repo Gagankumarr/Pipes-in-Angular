977231120d3b1ab1cf0347572d94aa9999e84b15
gagankumarrr <gkpippal@gmail.com>
2020-12-08 23:51:33 +0530

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appsix';
+  name="bruce"
+  disabledBox=true;
+  enableBox()
+  {
+    this.disabledBox=false
+  }
 }
